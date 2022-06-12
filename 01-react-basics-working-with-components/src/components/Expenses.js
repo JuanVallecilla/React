836a658cb03c4d52cc057649e71d65e03e1685b6
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-06-11 23:26:48 -0400

    the-concept-of-composition

--- 01-react-basics-working-with-components/src/components/Expenses.js
+++ 01-react-basics-working-with-components/src/components/Expenses.js
@@ -1,14 +1,14 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 const Expenses = (props) => {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.items.map((item) => (
         <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
       ))}
-      ;
-    </div>
+    </Card>
   );
 };
 
