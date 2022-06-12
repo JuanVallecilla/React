513e1852860cbb0564e8181cddd8ac10808eb255
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-06-11 23:36:24 -0400

    initial commit

---
+++ 02-react-state-events/01-working-with-state/src/components/Expenses/Expenses.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => (
+        <ExpenseItem key={item.title} title={item.title} amount={item.amount} date={item.date} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
