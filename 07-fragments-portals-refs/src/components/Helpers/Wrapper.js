05d8d800f796bea1570fe10a8773b0b6e485b678
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-06-14 17:20:15 -0400

    Creating a Wrapper Component

---
+++ 07-fragments-portals-refs/src/components/Helpers/Wrapper.js
@@ -0,0 +1,7 @@
+const Wrapper = (props) => {
+  // Remember the children prop holds all the content
+  // you're passing between the opening and closing tag
+  // off your custom component.
+  return props.children;
+};
+export default Wrapper;
