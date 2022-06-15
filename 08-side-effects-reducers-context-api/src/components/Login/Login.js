94527438dd91f89a2265536bf1cc25711ca1d985
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-06-15 18:47:09 -0400

    useEffect Summary

--- 08-side-effects-reducers-context-api/src/components/Login/Login.js
+++ 08-side-effects-reducers-context-api/src/components/Login/Login.js
@@ -11,6 +11,53 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // This effect fucntion runs after every
+  // component render cycle. Not before it
+  // and not during it, but after it.
+
+  // useEffect(() => {
+  //   console.log("Effect Running");
+  // });
+
+  // Now this function here, only executes
+  // for the first time this component was mounted and rendered,
+  // but not thereafter, not for any subsequent rerender cycle.
+
+  // useEffect(() => {
+  //   console.log("Effect Running");
+  // }, []);
+
+  // Now this function here, reruns whenever the
+  // component was re-evaluated and this state (enteredPassword),
+  // changes.
+
+  // useEffect(() => {
+  //   console.log("Effect Running");
+  // }, [enteredPassword]);
+
+  // The cleanup function, which we can return.
+  // This cleanup function runs before this state
+  // function(useEffect) in this case as a whole, runs,
+  // but not before the first time it runs.
+
+  // useEffect(() => {
+  //   console.log("Effect Running");
+  //   return () => {
+  //     console.log("Effect Clean Up");
+  //   };
+  // }, [enteredPassword]);
+
+  // if we had no dependencies we learned that we only see
+  // effect running once, and the cleanup function in this case,
+  // would run when the component is removed.
+
+  useEffect(() => {
+    console.log("Effect Running");
+    return () => {
+      console.log("Effect Clean Up");
+    };
+  }, []);
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity");
