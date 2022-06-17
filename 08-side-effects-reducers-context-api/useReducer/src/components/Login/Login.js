8c1e2cad8a31aceb426a38cc2d5d87a03fe190e1
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-06-17 02:36:14 -0400

    Diving into "Forward Refs"

--- 08-side-effects-reducers-context-api/useReducer/src/components/Login/Login.js
+++ 08-side-effects-reducers-context-api/useReducer/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -40,6 +40,9 @@ const Login = (props) => {
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: "", isValid: null });
 
   const authCtx = useContext(AuthContext);
+  const emailInputRef = useRef();
+  const passworrdInputRef = useRef();
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
@@ -75,13 +78,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passworrdInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-mail"
           type="email"
@@ -91,6 +101,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passworrdInputRef}
           id="password"
           label="Password"
           type="password"
@@ -100,7 +111,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- 08-side-effects-reducers-context-api/useReducer/src/components/UI/Input/input.js
+++ 08-side-effects-reducers-context-api/useReducer/src/components/UI/Input/input.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useRef, useImperativeHandle } from "react";
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const focus = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: focus,
+    };
+  });
+
   return (
     <div className={`${classes.control} ${props.isValid === false ? classes.invalid : ""}`}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} id={props.id} value={props.value} onChange={props.onChange} onBlur={props.onBlur} />
+      <input ref={inputRef} type={props.type} id={props.id} value={props.value} onChange={props.onChange} onBlur={props.onBlur} />
     </div>
   );
-};
+});
 
 export default Input;
