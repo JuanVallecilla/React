850f63d10adc69bce281455d48356600d343b7be
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-06-21 01:19:34 -0400

    Working with Refs & Forward Refs

--- 09-practice-food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ 09-practice-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 import styles from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+
+    if (enteredAmount.trim().length === 0 || enteredAmountNum < 1 || enteredAmountNum > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
         input={{
           id: "amount_" + props.id,
@@ -16,8 +32,10 @@ const MealItemForm = (props) => {
           defaultValue: 1,
         }}
         label="Amount"
+        ref={amountInputRef}
       />
       <button type="submit">Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
