1d6a2f35eb72bff822fd4c2c6c7d13bc68a23131
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-04 22:56:02 -0400

    Reading & Dispatching From A New Slice

--- 16-diving-into-redux/src/store/index.js
+++ 16-diving-into-redux/src/store/index.js
@@ -42,6 +42,6 @@ const store = configureStore({
 });
 
 export const counterActions = counterSlice.actions;
-export const authActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
