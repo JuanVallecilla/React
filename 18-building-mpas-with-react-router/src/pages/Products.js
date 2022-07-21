7e118f585afc1960d86eaab1d13b94aa3415ed2c
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-21 14:16:29 -0400

    Adding Dynamic Routes with Params

--- 18-building-mpas-with-react-router/src/pages/Products.js
+++ 18-building-mpas-with-react-router/src/pages/Products.js
@@ -1,5 +1,12 @@
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>A Book</ul>
+      <ul>Laptop</ul>
+      <ul>An Online Course</ul>
+    </section>
+  );
 };
 
 export default Products;
