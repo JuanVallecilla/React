13de3ba3f857972b155205f9426821ad20544a44
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-22 20:12:23 -0400

    Practicing Nested Routes

--- 19-building-mpas-with-react-router-practice/src/pages/QuoteDetail.js
+++ 19-building-mpas-with-react-router-practice/src/pages/QuoteDetail.js
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -9,6 +11,10 @@ const QuoteDetail = () => {
     <Fragment>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
+      {/* <Route path="/quotes/:quoteId/comments" exact> */}
+      <Route path={`/quotes/${params.quoteId}/comments`} exact>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
