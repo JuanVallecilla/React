5f905dc2f68b6fcfe5b7e92554816f7a500c6427
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-23 11:52:44 -0400

    Adding Dummy Data & More Content

--- 19-building-mpas-with-react-router-practice/src/pages/AllQuotes.js
+++ 19-building-mpas-with-react-router-practice/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: " Juan", text: "React is Fun!" },
+  { id: "q2", author: " Jhon", text: "React is not Fun!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
