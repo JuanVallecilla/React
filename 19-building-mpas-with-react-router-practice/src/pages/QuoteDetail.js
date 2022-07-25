7d6c67d046e7dde3cc94f9d2ccc23572297346a8
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-25 16:12:30 -0400

    Sending & Getting Quote Data via Http

--- 19-building-mpas-with-react-router-practice/src/pages/QuoteDetail.js
+++ 19-building-mpas-with-react-router-practice/src/pages/QuoteDetail.js
@@ -1,20 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
+
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: " Juan", text: "React is Fun!" },
-  { id: "q2", author: " Jhon", text: "React is not Fun!" },
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
-  // console.log(match);
-  const params = useParams();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = useParams();
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
 
-  if (!quote) {
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
@@ -22,7 +36,7 @@ const QuoteDetail = () => {
   // console.log(quote);
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
