4f09d2a5be92d2a696ee24ac4415406f67174b3d
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-29 14:35:06 -0400

    Adding Auto-Logout

--- 21-authentication/src/components/Auth/AuthForm.js
+++ 21-authentication/src/components/Auth/AuthForm.js
@@ -57,7 +57,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        authCtx.login(data.idToken, Date.now() + data.expiresIn * 1000);
         history.replace("/");
       })
       .catch((error) => {
