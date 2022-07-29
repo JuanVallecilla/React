2047b6ee4571399d9eb0578ee417d02f6c15fbbc
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-29 13:23:03 -0400

    Redirecting The User

--- 21-authentication/src/components/Auth/AuthForm.js
+++ 21-authentication/src/components/Auth/AuthForm.js
@@ -1,4 +1,5 @@
 import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
@@ -7,6 +8,7 @@ const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -56,6 +58,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace("/");
       })
       .catch((error) => {
         alert(error.message);
