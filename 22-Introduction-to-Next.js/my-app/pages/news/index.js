66afdc0a55b3b0e555bd0f4f7ee8d5860c580168
Juan Vallecilla <37926618+JuanVallecilla@users.noreply.github.com>
2022-07-30 19:23:03 -0400

    Linking Between Pages

--- 22-Introduction-to-Next.js/my-app/pages/news/index.js
+++ 22-Introduction-to-Next.js/my-app/pages/news/index.js
@@ -1,7 +1,19 @@
 // our-domain.com/news
+import Link from "next/link";
+import { Fragment } from "react";
 
 const newsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">NextJS Is A Great FrameEork</Link>
+        </li>
+        <li>NextJS Is Awesome </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default newsPage;
