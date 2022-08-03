f13f57d56d195bc310ddcaa594d2e1701aaf3da4
Juan <juanmanuelvallecilla@hotmail.ca>
2022-08-03 18:07:07 -0400

    Data Fetching for Static Pages

--- 23-NextJs-continuation/my-app/pages/index.js
+++ 23-NextJs-continuation/my-app/pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -18,17 +18,19 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-  useEffect(() => {
-    // send a http request and fetch data
-    // The prerender page genereated by nextJs will take
-    // the result of the first render cycle
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
-
-  return <MeetupList meetups={loadedMeetups} />;
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+export async function getStaticProps() {
+  // fetch data from an API
+  // You ALWAYS need to return an object here
+  return {
+    // has to be name props
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
 export default HomePage;
