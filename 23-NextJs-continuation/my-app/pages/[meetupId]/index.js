889b824f38776709f788520b4841b2942859708c
Juan <juanmanuelvallecilla@hotmail.ca>
2022-08-04 13:38:11 -0400

    Working with Params for SSG Data Fetching

--- 23-NextJs-continuation/my-app/pages/[meetupId]/index.js
+++ 23-NextJs-continuation/my-app/pages/[meetupId]/index.js
@@ -11,4 +11,22 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  //fetch data for a single meetup
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some address 5, 1234 Some City",
+        description: "The meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
