78c40a7133b8fb2c2b6731b75ed6bda28637587f
Juan <juanmanuelvallecilla@hotmail.ca>
2022-08-04 16:42:58 -0400

    Working with MongoDB

--- 23-NextJs-continuation/my-app/pages/api/new-meetup.js
+++ 23-NextJs-continuation/my-app/pages/api/new-meetup.js
@@ -1,10 +1,23 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 //  POST /api/new-meetup
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(process.env.DB_URL);
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup Inserted!" });
   }
 }
 
